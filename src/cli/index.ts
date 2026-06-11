bf12883307e340919aa65166504130f9b463da61
serra-joan <joanserra0501@gmail.com>
2026-06-11 19:13:21 +0200

    eliminat imports

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-import fs from 'node:fs';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import pc from 'picocolors';
 import { run } from './run.js';
 import { help, commands, version } from '../utils/common_prints.js';
